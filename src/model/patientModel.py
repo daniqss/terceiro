518e79eaba9c1eb06c4a841c28b8145e8790f9d7
daniqss <danielqueijo14@gmail.com>
2024-10-09 08:58:17 +0200

    complete CRUD in patients model

--- src/model/patientModel.py
+++ src/model/patientModel.py
@@ -3,7 +3,33 @@ from utils import request_data, PORT
 
 class PatientModel:
     @staticmethod
-    def getPatients() -> Optional[dict]:
+    def getPatients() -> Optional[list]:
         url = f"http://localhost:{PORT}/patients"
-        return request_data(url, "GET")
+        patients, status = request_data(url, "GET")
+        if status is 200:
+            return patients
+        return None
+
+    def addPatient(id: int, code: str, name: str, surname: str) -> Optional[dict]:
+        url = f"http://localhost:{PORT}/patients"
+        data = {"id": id, "code": code, "name": name, "surname": surname}
+        response, status = request_data(url, "POST", data)
+        if status is 201:
+            return response
+        return None
+    
+    def updatePatient(id: int, code: str, name: str, surname: str) -> Optional[dict]:
+        url = f"http://localhost:{PORT}/patients/{id}"
+        data = {"id": id, "code": code, "name": name, "surname": surname}
+        response, status = request_data(url, "PATCH", data)
+        if status is 204:
+            return response
+        return None
+    
+    def deletePatient(id: int) -> Optional[dict]:
+        url = f"http://localhost:{PORT}/patients/{id}"
+        response, status = request_data(url, "DELETE")
+        if status is 204:
+            return response
+        return None
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
+from typing import Optional
 from requests import request
+from requests.exceptions import RequestException
 
-def request_data(url: str, method: str = "GET") -> dict:
-    response = request(method, url)
-    return response.json()
+def request_data(url: str, method: str = "GET") -> tuple[Optional[dict | list], int | None]:
+    try: 
+        response = request(method, url)
+        return response.json(), response.status_code
+    
+    except RequestException as e:
+        print(f"An error occurred: {e}")
+        return None, None
 
 PORT: int = 8000
