2223ba3174ba0ff9da8fae68ca5727df92a464c1
daniqss <danielqueijo14@gmail.com>
2024-10-19 22:32:06 +0200

    style(#13): better solution for response.json() error handling
    close #13

--- src/utils.py
+++ src/utils.py
@@ -2,28 +2,27 @@ import json
 from os import getenv
 from typing import Optional
 from requests import request
-from requests.exceptions import RequestException
-# from requests.exceptions import JSONDecodeError
-from src.exceptions import NetworkErrorException
+from requests.exceptions import RequestException, JSONDecodeError
+from src.exceptions import NetworkErrorException, DataErrorException
 
 def request_data(url: str, method: str = "GET", data: Optional[dict] = None) -> tuple[dict | list, int]:
     try: 
         response = request(
             method=method,
             url=url,
-            headers={"Content-Type": "application/json"} if method in ["PATCH", "POST"] else None,  # Arreglo de headers
+            headers={"Content-Type": "application/json"} if method in ["PATCH", "POST"] else None,
             data=json.dumps(data) if data is not None else None 
         )
-        
-        #FIXME super horrible solution, we must fix this problem with the .json() exceptions
-        # but works for now
-        if response.status_code == 204:
+
+        try:
+            return response.json(), response.status_code
+        except JSONDecodeError:
             return {}, 204
         
-        return response.json(), response.status_code
     except RequestException as e:
-        print(f"Error: {e}")
-        raise NetworkErrorException("Error de red")
+        raise NetworkErrorException(e)
+    except Exception as e:
+        raise DataErrorException(e)
 
 PORT: int = int(getenv("PORT", 8000))
 HOST: str = getenv("HOST", "localhost")
