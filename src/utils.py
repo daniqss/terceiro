976ab2b1ebd56a471bb3cd9bcb9490b4f9afd37a
daniqss <danielqueijo14@gmail.com>
2024-10-20 20:33:56 +0200

    style(#16): fix typo, from funcion to function
    close #16

--- src/utils.py
+++ src/utils.py
@@ -44,5 +44,5 @@ def request_data(url: str, method: str = "GET", data: Optional[dict] = None) -> 
 PORT: int = int(getenv("PORT", 8000))
 APPLICATION_ID: str = "es.udc.fic.ipm.acdc.pacientes"
 WINDOW_PADDING: int = 24
-BLOCK_TIME: int = int(getenv("BLOCK_TIME", 0)) # Used for testing asynchronous funcions
+BLOCK_TIME: int = int(getenv("BLOCK_TIME", 0)) # Used for testing asynchronous functions
 ASYNC_CODE: bool = bool(getenv("ASYNC_CODE", True))
