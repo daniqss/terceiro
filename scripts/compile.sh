035a814cd41e12de0792b5c3b45aa0a21b2aa23e
daniqss <danielqueijo14@gmail.com>
2024-10-26 17:58:31 +0200

    build: update compile.sh
    solve 'msgmerge: exactly 2 input files required'

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,13 +3,11 @@
 echo "Generating the POT file..."
 xgettext -o locales/patients-acdc.pot --from-code=UTF-8 src/*.py
 
-echo "Updating PO files..."
-msgmerge --update --backup=off locales/*.po locales/patients-acdc.pot
-
 echo "Compiling MO files..."
 for lang in locales/*.po; do
     lang_dir="locales/$(basename "$lang" .po)/LC_MESSAGES"
     mkdir -p "$lang_dir"  # Crear el directorio si no existe
+    msgmerge --update --backup=off $lang locales/patients-acdc.pot
     msgfmt "$lang" -o "$lang_dir/patients-acdc.mo"
 done
 
