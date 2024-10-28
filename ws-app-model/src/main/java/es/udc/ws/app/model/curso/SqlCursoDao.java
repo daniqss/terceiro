b79404a87e0c5f3116332c5ba45f318636688727
daniqss <danielqueijo14@gmail.com>
2024-10-28 12:14:05 +0100

    [VARIOS] Implementada interfaz para el DAO de Curso.

--- ws-app-model/src/main/java/es/udc/ws/app/model/curso/SqlCursoDao.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/curso/SqlCursoDao.java
@@ -1,4 +1,19 @@
 package es.udc.ws.app.model.curso;
 
-public class SqlCursoDao {
+import es.udc.ws.util.exceptions.InputValidationException;
+
+import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface SqlCursoDao {
+    public Curso crear(Connection conexion, Curso curso) throws InputValidationException;
+    public Curso borrar(Connection conexion, Long cursoId) throws InputValidationException;
+    public Curso actualizar(Connection conexion, Curso curso) throws InputValidationException;
+    public Curso buscarPorId(Connection conexion, Curso curso) throws InputValidationException;
+    public List<Curso> buscarPorCiudadYFecha(
+            Connection conexion,
+            String ciudad,
+            LocalDateTime fecha
+    ) throws InputValidationException;
 }
