13279782b4ef6c63edda2a1d8bd81d2a3c1bff38
daniqss <danielqueijo14@gmail.com>
2024-11-03 20:06:54 +0100

    [VARIOS] Añadida factoría para el DAO de Curso

--- ws-app-model/src/main/java/es/udc/ws/app/model/curso/SqlCursoDaoFactory.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/curso/SqlCursoDaoFactory.java
@@ -1,4 +1,30 @@
 package es.udc.ws.app.model.curso;
 
+import es.udc.ws.util.configuration.ConfigurationParametersManager;
+
 public class SqlCursoDaoFactory {
+    private final static String CLASS_NAME_PARAMETER = "SqlCursoDaoFactory.className";
+    private static SqlCursoDao instaciaDao = null;
+
+    private SqlCursoDaoFactory() { }
+
+    private static SqlCursoDao getInstance() {
+        try {
+            String daoClassName = ConfigurationParametersManager.getParameter(CLASS_NAME_PARAMETER);
+            Class daoClass = Class.forName(daoClassName);
+            return (SqlCursoDao) daoClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /*
+     synchronized keyword means that only one thread is allowed
+     at a particular time to complete a given task entirely
+     */
+    public synchronized static SqlCursoDao getDao() {
+        if (instaciaDao == null) instaciaDao = getInstance();
+        return instaciaDao;
+    }
 }
