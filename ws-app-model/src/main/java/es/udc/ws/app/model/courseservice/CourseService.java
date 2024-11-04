c31d8b14cf34e1f34010b38a0aa2442012573d21
daniqss <danielqueijo14@gmail.com>
2024-11-04 12:18:41 +0100

    [VARIOS] Añadida estructura de courseservice y interfaz CourseService

---
+++ ws-app-model/src/main/java/es/udc/ws/app/model/courseservice/CourseService.java
@@ -0,0 +1,54 @@
+package es.udc.ws.app.model.courseservice;
+
+import es.udc.ws.app.model.course.Course;
+import es.udc.ws.app.model.inscription.Inscription;
+
+import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface CourseService {
+    /*
+    To register a course, you must indicate the city where it is held, its name, the date and time of start,
+    the price and the maximum number of places. In addition, the date and time at which the course was registered will
+     be saved. The start date of the course must be at least 15 days after the time of registration.
+     */
+    public Course addCourse(String city, LocalDateTime startDate, BigDecimal price, int maxVacantSpots);
+
+    /*
+    It will be possible to search for courses that are held in a city from a date. Only courses that are held in that
+    city and whose start date is later than the indicated date will be returned, ordered by date of celebration.
+    The information returned for the courses will include, in addition to the rest of the data, the number of places
+    available at that time.
+    */
+    public List<Course> findCourses(String city, LocalDateTime date);
+
+    /*
+    It will be possible to search for courses by their identifier. As in the previous point, the information returned
+    for the course will include, in addition to the rest of the data, the number of places available at that time.
+     */
+    public Course findCourse(Long courseId);
+
+    /*
+    A user may register for a course as long as the registration period is still open (i.e. the course has not yet started).
+     In addition to any other parameters that may be necessary, the user receives an email to identify the user and a
+      bank card number to make the payment. If successful, the registration is stored, recording the date and time at
+       which it was made, and the identifier assigned to it is returned.
+     */
+    public Long addInscription(Long courseId, String userEmail, String bankCardNumber);
+
+    /*
+    A user can cancel a registration as long as the cancellation period is still open (i.e., there are more than 7 days
+    left until the start of the course), and it has not already been cancelled. The user receives as input the ID of
+    the registration that he/she wishes to cancel and the email of the user who is cancelling the registration
+    (a user can only cancel his/her registrations). If successful, the date and time at which the registration was
+    cancelled will be recorded.
+    */
+    public Inscription cancelInscription(Long inscriptionId, String userEmail);
+
+    /*
+    It will be possible to retrieve all the registrations that a user has made over time, sorted by the date the
+    registration was created (most recent first).All data stored for each registration must be returned.
+    */
+    public List<Inscription> findInscriptions(String userEmail);
+}
---
+++ ws-app-model/src/main/java/es/udc/ws/app/model/courseservice/CourseServiceFactory.java
@@ -0,0 +1,4 @@
+package es.udc.ws.app.model.courseservice;
+
+public class CourseServiceFactory {
+}
