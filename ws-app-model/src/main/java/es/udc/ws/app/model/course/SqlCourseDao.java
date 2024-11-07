421b7f3ca63128a3fd97bd64c21fd3fd00544037
daniqss <danielqueijo14@gmail.com>
2024-11-07 20:00:41 +0100

    [VARIOS] Cambiada las excepciones que puede lanzar SqlCourseDao de InputValidationException a RuntimeException

--- ws-app-model/src/main/java/es/udc/ws/app/model/course/SqlCourseDao.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/course/SqlCourseDao.java
@@ -7,13 +7,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SqlCourseDao {
-    public Course create(Connection connection, Course course) throws InputValidationException;
-    public void remove(Connection connection, Long courseId) throws InputValidationException;
-    public Course update(Connection connection, Course course) throws InputValidationException;
-    public Course findById(Connection connection, Long courseId) throws InputValidationException;
+    public Course create(Connection connection, Course course) throws RuntimeException;
+    public void remove(Connection connection, Long courseId) throws RuntimeException;
+    public Course update(Connection connection, Course course) throws RuntimeException;
+    public Course findById(Connection connection, Long courseId) throws RuntimeException;
     public List<Course> findByKeyword(
             Connection connection,
             String city,
             LocalDateTime date
-    ) throws InputValidationException;
+    ) throws RuntimeException;
 }
