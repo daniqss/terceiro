03c11d520e66993cc9a4c3c0b8a7b70b38d44295
paulacarril <145045483+paulacarril@users.noreply.github.com>
2024-11-09 20:03:17 +0100

    [VARIOS] Implementado script de creacion de las tablas SQL Course y Inscription

--- ws-app-model/src/sql/MySQLCreateTables.sql
+++ ws-app-model/src/sql/MySQLCreateTables.sql
@@ -1,3 +1,28 @@
 -- ----------------------------------------------------------------------------
 -- Model
 -------------------------------------------------------------------------------
+DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS Inscription;
+
+
+CREATE TABLE Course (
+    courseId BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    city VARCHAR(200) NOT NULL,
+    creationDate DATETIME NOT NULL,
+    startDate DATETIME NOT NULL,
+    price FLOAT NOT NULL,
+    maxSpots INT NOT NULL,
+    vacantSpots INT NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE Inscription (
+    inscriptionId BIGINT NOT NULL AUTO_INCREMENT,
+    courseId BIGINT NOT NULL,
+    inscriptionDate DATETIME NOT NULL,
+    cancelationDate DATETIME,
+    userEmail VARCHAR(200) NOT NULL,
+    CONSTRAINT InscriptionPK PRIMARY KEY (inscriptionId),
+    CONSTRAINT CourseFK FOREIGN KEY (courseId)
+    REFERENCES Course (courseId) ON DELETE CASCADE
+) ENGINE = InnoDB;
