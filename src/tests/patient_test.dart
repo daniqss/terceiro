33f10b871c8d7331f80ff13756ec7e41225aa974
javiermanotasruiz <j.manotas@outlook.es>
2024-11-29 08:45:31 +0000

    test: implement basic file structure

---
+++ src/tests/patient_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tests/integration_test.dart';
+import 'package:src/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Load patient data and filter medications', (WidgetTester tester) async {
+     // TODO
+  });
+}
