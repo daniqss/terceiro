5de5997c79be1d088f1cb08620ad842084f2af5b
javiermanotasruiz <j.manotas@outlook.es>
2024-12-01 21:09:33 +0000

    test: implement login tests

---
+++ src/integration_test/app_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:provider/provider.dart';
+import 'package:src/main.dart';
+import 'package:src/model.dart';
+import 'package:src/providers/login_provider.dart';
+
+Future<String> getTestCode() async {
+  final PatientModel model = PatientModel();
+
+  final List<Map<String, dynamic>> patients = await model.getPatients();
+  return patients[0]["code"];
+}
+
+void main() {
+
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Login Integration Tests', ()
+  {
+
+    testWidgets('Login with invalid codes', (WidgetTester tester) async {
+
+      await tester.pumpWidget(MyApp());
+
+      expect(find.text('Bienvenido a SergasProMax'), findsOneWidget);
+
+      final patientCodeInput = find.byType(TextField);
+      expect(patientCodeInput, findsOneWidget);
+
+      final loginButton = find.text('Entrar');
+      expect(loginButton, findsOneWidget);
+
+      await tester.enterText(patientCodeInput, 'invalid_code');
+      await tester.pumpAndSettle();
+
+      await tester.tap(loginButton);
+      await tester.pump();
+
+      expect(find.byType(SnackBar), findsOneWidget);
+    });
+
+    /*testWidgets('Login with valid code', (WidgetTester tester) async {
+
+      await tester.pumpWidget(MyApp());
+
+      final patientCodeInput = find.byType(TextField);
+      expect(patientCodeInput, findsOneWidget);
+
+      final loginButton = find.text('Entrar');
+      expect(loginButton, findsOneWidget);
+
+
+      // valid login
+      final code = await getTestCode();
+
+      await tester.enterText(patientCodeInput, code);
+      await tester.pumpAndSettle();
+
+      await tester.tap(loginButton);
+      await tester.pump();
+
+      await Future.delayed(Duration(seconds: 4));
+
+    });*/
+  });
+}
