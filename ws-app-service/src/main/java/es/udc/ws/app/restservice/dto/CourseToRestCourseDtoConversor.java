e6123918f97e08cda7fd6b039e5574c08058f96b
jnoguevera <146925644+jnoguevera@users.noreply.github.com>
2024-12-09 18:07:40 +0100

    [VARIOS] Implementado CourseToRest

--- ws-app-service/src/main/java/es/udc/ws/app/restservice/dto/CourseToRestCourseDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/app/restservice/dto/CourseToRestCourseDtoConversor.java
@@ -1,4 +1,26 @@
 package es.udc.ws.app.restservice.dto;
 
+import es.udc.ws.app.model.course.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourseToRestCourseDtoConversor {
+    public static List<RestCourseDto> toRestCourseDtos(List<Course> courses) {
+        List<RestCourseDto> courseDtos = new ArrayList<>(courses.size());
+        for (int i = 0; i < courses.size(); i++) {
+            Course course = courses.get(i);
+            courseDtos.add(toRestCourseDto(course));
+        }
+        return courseDtos;
+    }
+
+
+    public static RestCourseDto toRestCourseDto(Course course) {
+        return new RestCourseDto(course.getCourseId(), course.getName(), course.getCity(), course.getStartDate(), course.getPrice(), course.getMaxSpots(), course.getVacantSpots());
+    }
+
+    public static Course toCourse(RestCourseDto course) {
+        return new Course(course.getName(), course.getCity(), course.getStartDate(), course.getPrice(), course.getMaxSpots());
+    }
 }
