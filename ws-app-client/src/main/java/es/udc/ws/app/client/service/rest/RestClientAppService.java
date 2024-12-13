d303dbf453a3ec1762e19791af85dfc9e7f32d9b
paulacarril <145045483+paulacarril@users.noreply.github.com>
2024-12-13 20:08:01 +0100

    [VARIOS] Implementadas funciones de conexion en Client service

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/rest/RestClientAppService.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/rest/RestClientAppService.java
@@ -1,4 +1,119 @@
 package es.udc.ws.app.client.service.rest;
 
-public class RestClientAppService {
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import es.udc.ws.app.client.service.ClientAppService;
+import es.udc.ws.app.client.service.dto.ClientCourseDto;
+import es.udc.ws.app.client.service.dto.ClientInscriptionDto;
+import es.udc.ws.app.client.service.exceptions.*;
+import es.udc.ws.app.client.service.rest.json.JsonToClientExceptionDtoConversor;
+import es.udc.ws.app.client.service.rest.json.JsonToClientInscriptionDtoConversor;
+import es.udc.ws.util.configuration.ConfigurationParametersManager;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import es.udc.ws.util.json.ObjectMapperFactory;
+import org.apache.hc.client5.http.fluent.Form;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpStatus;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class RestClientAppService implements ClientAppService {
+    private final static String ENDPOINT_ADDRESS_PARAMETER = "RestClientAppService.endpointAddress";
+    private String endpointAddress;
+
+    @Override
+    public ClientCourseDto addCourse(ClientCourseDto course) throws InputValidationException, RuntimeException, ClientCourseStartTooSoonException {
+        return null;
+    }
+
+    @Override
+    public List<ClientCourseDto> findCourses(String city, LocalDateTime date) throws RuntimeException, InputValidationException {
+        return List.of();
+    }
+
+    @Override
+    public ClientCourseDto findCourse(Long courseId) throws InputValidationException, InstanceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ClientInscriptionDto addInscription(Long courseId, String userEmail, String bankCardNumber) throws InputValidationException, InstanceNotFoundException, ClientCourseAlreadyStartedException, ClientCourseFullException {
+        return null;
+    }
+
+    @Override
+    public void cancelInscription(Long inscriptionId, String userEmail) throws RuntimeException, InstanceNotFoundException, InputValidationException, ClientIncorrectUserException, ClientInscriptionAlreadyCancelledException, ClientCancelTooCloseToCourseStartException{
+    }
+
+    @Override
+    public List<ClientInscriptionDto> findInscriptions(String userEmail) {
+        return List.of();
+    }
+
+
+    private synchronized String getEndpointAddress() {
+        if (endpointAddress == null) {
+            endpointAddress = ConfigurationParametersManager
+                    .getParameter(ENDPOINT_ADDRESS_PARAMETER);
+        }
+        return endpointAddress;
+    }
+
+    private InputStream toInputStream(ClientCourseDto course) {
+
+        try {
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            objectMapper.writer(new DefaultPrettyPrinter()).writeValue(outputStream,
+                    JsonToClientCourseDtoConversor.toObjectNode(course));
+
+            return new ByteArrayInputStream(outputStream.toByteArray());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void validateStatusCode(int successCode, ClassicHttpResponse response) throws Exception {
+
+        try {
+
+            int statusCode = response.getCode();
+
+            /* Success? */
+            if (statusCode == successCode) {
+                return;
+            }
+
+            /* Handler error. */
+            switch (statusCode) {
+                case HttpStatus.SC_NOT_FOUND -> throw JsonToClientExceptionDtoConversor.fromNotFoundErrorCode(
+                        response.getEntity().getContent());
+                case HttpStatus.SC_BAD_REQUEST -> throw JsonToClientExceptionDtoConversor.fromBadRequestErrorCode(
+                        response.getEntity().getContent());
+                case HttpStatus.SC_FORBIDDEN -> throw JsonToClientExceptionDtoConversor.fromForbiddenErrorCode(
+                        response.getEntity().getContent());
+                case HttpStatus.SC_GONE -> throw JsonToClientExceptionDtoConversor.fromGoneErrorCode(
+                        response.getEntity().getContent());
+                default -> throw new RuntimeException("HTTP error; status code = "
+                        + statusCode);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
+
