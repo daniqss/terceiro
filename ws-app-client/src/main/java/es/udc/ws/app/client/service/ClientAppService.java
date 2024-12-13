f3591c6d885ff030ce7a92d9dc3271149d09d9c1
paulacarril <145045483+paulacarril@users.noreply.github.com>
2024-12-13 17:01:52 +0100

    [VARIOS] Implementada interface service para el client

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/ClientAppService.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/ClientAppService.java
@@ -1,4 +1,23 @@
 package es.udc.ws.app.client.service;
 
+import es.udc.ws.app.client.service.dto.ClientCourseDto;
+import es.udc.ws.app.client.service.dto.ClientInscriptionDto;
+import es.udc.ws.app.client.service.exceptions.*;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ClientAppService {
+    ClientCourseDto addCourse(ClientCourseDto course) throws InputValidationException, RuntimeException, ClientCourseStartTooSoonException;
+
+    List<ClientCourseDto> findCourses(String city, LocalDateTime date) throws RuntimeException, InputValidationException;
+
+    ClientCourseDto findCourse(Long courseId) throws InputValidationException, InstanceNotFoundException;
+
+    ClientInscriptionDto addInscription(Long courseId, String userEmail, String bankCardNumber) throws InputValidationException, InstanceNotFoundException, ClientCourseAlreadyStartedException, ClientCourseFullException;
+
+    void cancelInscription(Long inscriptionId, String userEmail) throws RuntimeException, InstanceNotFoundException, InputValidationException, ClientIncorrectUserException, ClientInscriptionAlreadyCancelledException, ClientCancelTooCloseToCourseStartException;
+
+    List<ClientInscriptionDto> findInscriptions(String userEmail);
 }
