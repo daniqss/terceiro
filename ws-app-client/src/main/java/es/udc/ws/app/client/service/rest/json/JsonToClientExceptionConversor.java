424e4cb4012b1637e59ff0ef1ba4ed22f94b1e2b
paulacarril <145045483+paulacarril@users.noreply.github.com>
2024-12-13 18:42:53 +0100

    [VARIOS] Implementado json exception conversor del cliente

---
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/rest/json/JsonToClientExceptionConversor.java
@@ -0,0 +1,137 @@
+package es.udc.ws.app.client.service.rest.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import es.udc.ws.app.client.service.exceptions.*;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import es.udc.ws.util.json.ObjectMapperFactory;
+import es.udc.ws.util.json.exceptions.ParsingException;
+import java.io.InputStream;
+
+public class JsonToClientExceptionConversor {
+
+    public static Exception fromBadRequestErrorCode(InputStream ex) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(ex);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+                if (errorType.equals("InputValidation")) {
+                    return toInputValidationException(rootNode);
+                } else {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+    private static InputValidationException toInputValidationException(JsonNode rootNode) {
+        String message = rootNode.get("message").textValue();
+        return new InputValidationException(message);
+    }
+
+    public static Exception fromNotFoundErrorCode(InputStream ex) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(ex);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+                if (errorType.equals("InstanceNotFound")) {
+                    return toInstanceNotFoundException(rootNode);
+                } else {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+    private static InstanceNotFoundException toInstanceNotFoundException(JsonNode rootNode) {
+        String instanceId = rootNode.get("instanceId").textValue();
+        String instanceType = rootNode.get("instanceType").textValue();
+        return new InstanceNotFoundException(instanceId, instanceType);
+    }
+
+    public static Exception fromForbiddenErrorCode(InputStream ex) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(ex);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+                if (errorType.equals("CourseFull")) {
+                    return toCourseFullException(rootNode);
+                } else if (errorType.equals("IncorrectUser")) {
+                    return toIncorrectUserException(rootNode);
+                } else if (errorType.equals("CancelTooCloseToCourseStart")) {
+                    return toCancelTooCloseToCourseStartException(rootNode);
+                }
+                else {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+    private static ClientCourseFullException toCourseFullException(JsonNode rootNode) {
+        return new ClientCourseFullException();
+    }
+
+    private static ClientIncorrectUserException toIncorrectUserException(JsonNode rootNode) {
+        return new ClientIncorrectUserException();
+    }
+
+    private static ClientCancelTooCloseToCourseStartException toCancelTooCloseToCourseStartException(JsonNode rootNode) {
+        return new ClientCancelTooCloseToCourseStartException();
+    }
+
+    public static Exception fromGoneErrorCode(InputStream ex) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(ex);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+
+                if (errorType.equals("CourseStartTooSoon")) {
+                    return toCourseStartTooSoonException(rootNode);
+                } else if (errorType.equals("CourseAlreadyStarted"))
+                    return toCourseAlreadyStartedException(rootNode);
+                {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+    private static ClientCourseStartTooSoonException toCourseStartTooSoonException(JsonNode rootNode) {
+        return new ClientCourseStartTooSoonException();
+    }
+
+    private static ClientCourseAlreadyStartedException toCourseAlreadyStartedException(JsonNode rootNode) {
+        return new ClientCourseAlreadyStartedException();
+    }
+}
