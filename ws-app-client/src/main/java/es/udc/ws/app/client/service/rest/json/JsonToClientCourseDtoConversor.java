41c5b74c39603ae725e2e6e7a27795d6831c3884
daniqss <danielqueijo14@gmail.com>
2024-12-14 19:50:54 +0100

    [VARIOS] Agregado toClientCourseDtos en JsonToClientCourseDtoConversor

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/rest/json/JsonToClientCourseDtoConversor.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/rest/json/JsonToClientCourseDtoConversor.java
@@ -2,6 +2,7 @@ package es.udc.ws.app.client.service.rest.json;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -13,6 +14,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonToClientCourseDtoConversor {
     public static ObjectNode toObjectNode(ClientCourseDto course) throws IOException {
@@ -30,6 +33,50 @@ public class JsonToClientCourseDtoConversor {
                 put("vacantSpots", course.getVacantSpots())
         ;
         return courseObject;
+    }
+
+    public static List<ClientCourseDto> toClientCourseDtos(InputStream jsonCourses) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(jsonCourses);
+
+            if (rootNode.getNodeType() != JsonNodeType.ARRAY) {
+                throw new ParsingException("Unrecognized JSON (array expected)");
+            } else {
+                ArrayNode coursesArray = (ArrayNode) rootNode;
+                List<ClientCourseDto> courseDtos = new ArrayList<>(coursesArray.size());
+                for (JsonNode courseNode : coursesArray) {
+                    courseDtos.add(toClientCourseDto(courseNode));
+                }
+
+                return courseDtos;
+            }
+        } catch (ParsingException ex) {
+            throw ex;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+    private static ClientCourseDto toClientCourseDto(JsonNode courseNode) throws ParsingException {
+        if (courseNode.getNodeType() != JsonNodeType.OBJECT) {
+            throw new ParsingException("Unrecognized JSON (object expected)");
+        } else {
+            ObjectNode courseObject = (ObjectNode) courseNode;
+
+            JsonNode courseIdNode = courseObject.get("courseId");
+            Long courseId = (courseIdNode != null) ? courseIdNode.longValue() : null;
+
+            String name = courseObject.get("name").textValue().trim();
+            String city = courseObject.get("city").textValue().trim();
+            LocalDateTime startDate = LocalDateTime.parse(courseObject.get("startDate").textValue().trim(),
+                    DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            float price = (float) courseObject.get("price").doubleValue();
+            int maxSpots = courseObject.get("maxSpots").intValue();
+            int vacantSpots = courseObject.get("vacantSpots").intValue();
+
+            return new ClientCourseDto(courseId, name, city, startDate, price, maxSpots, maxSpots - vacantSpots);
+        }
     }
 
     public static ClientCourseDto toClientCourseDto(InputStream jsonCourse) throws ParsingException {
