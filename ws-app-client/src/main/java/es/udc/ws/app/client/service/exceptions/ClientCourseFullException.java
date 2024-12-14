344c6836d6e9bc9d4fdb9e50e39678b4ef145077
paulacarril <145045483+paulacarril@users.noreply.github.com>
2024-12-14 19:09:10 +0100

    [VARIOS] Implementadas excepciones para el client

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/exceptions/ClientCourseFullException.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/exceptions/ClientCourseFullException.java
@@ -1,7 +1,18 @@
 package es.udc.ws.app.client.service.exceptions;
 
 public class ClientCourseFullException extends RuntimeException {
-    public ClientCourseFullException(String message) {
-        super(message);
+    private Long courseId;
+
+    public ClientCourseFullException(Long courseId) {
+        super("Al curso con id = " + courseId + " no le quedan plazas disponibles");
+        this.courseId = courseId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long idCourse) {
+        this.courseId = idCourse;
     }
 }
