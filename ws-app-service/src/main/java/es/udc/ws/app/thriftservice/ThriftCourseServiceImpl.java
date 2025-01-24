394061a7117209386b110e99ad080a2c606ac951
paulacarril <145045483+paulacarril@users.noreply.github.com>
2025-01-24 19:11:31 +0100

    [VARIOS] Añadidas cabeceras de las funciones a ThriftCourseServiceImpl

--- ws-app-service/src/main/java/es/udc/ws/app/thriftservice/ThriftCourseServiceImpl.java
+++ ws-app-service/src/main/java/es/udc/ws/app/thriftservice/ThriftCourseServiceImpl.java
@@ -1,4 +1,38 @@
 package es.udc.ws.app.thriftservice;
 
-public class ThriftCourseServiceImpl {
+import es.udc.ws.app.thrift.*;
+import org.apache.thrift.TException;
+
+import java.util.List;
+
+public class ThriftCourseServiceImpl implements ThriftCourseService.Iface {
+    @Override
+    public ThriftCourseDto addCourse(ThriftCourseDto courseDto) throws ThriftInputValidationException, ThriftCourseStartTooSoonException, TException {
+        return null;
+    }
+
+    @Override
+    public List<ThriftCourseDto> findCourses(String city) throws ThriftInputValidationException, TException {
+        return List.of();
+    }
+
+    @Override
+    public ThriftCourseDto findCourse(long courseId) throws ThriftInputValidationException, ThriftInstanceNotFoundException, TException {
+        return null;
+    }
+
+    @Override
+    public ThriftInscriptionDto addInscription(long courseId, String userEmail, String bankCardNumber) throws ThriftInputValidationException, ThriftInstanceNotFoundException, ThriftCourseAlreadyStartedException, ThriftCourseFullException, TException {
+        return null;
+    }
+
+    @Override
+    public void cancelInscription(long inscriptionId, String userEmail) throws ThriftInstanceNotFoundException, ThriftInputValidationException, ThriftIncorrectUserException, ThriftInscriptionAlreadyCancelledException, ThriftCancelTooCloseToCourseStartException, TException {
+
+    }
+
+    @Override
+    public List<ThriftInscriptionDto> findInscriptions(String userEmail) throws ThriftInputValidationException, TException {
+        return List.of();
+    }
 }
