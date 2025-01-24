b630c46503389390daadde66b35c654a282b61da
paulacarril <145045483+paulacarril@users.noreply.github.com>
2025-01-24 17:24:05 +0100

    [VARIOS] Implementado ThriftCourseServiceServlet

--- ws-app-service/src/main/java/es/udc/ws/app/thriftservice/ThriftCourseServiceServlet.java
+++ ws-app-service/src/main/java/es/udc/ws/app/thriftservice/ThriftCourseServiceServlet.java
@@ -1,4 +1,24 @@
 package es.udc.ws.app.thriftservice;
 
-public class ThriftCourseServiceServlet {
+import es.udc.ws.app.thrift.ThriftCourseService;
+import es.udc.ws.util.servlet.ThriftHttpServletTemplate;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+
+public class ThriftCourseServiceServlet extends ThriftHttpServletTemplate {
+    public ThriftCourseServiceServlet() {
+        super(createProcessor(), createProtocolFactory());
+    }
+
+    private static TProcessor createProcessor() {
+
+        return new ThriftCourseService.Processor<ThriftCourseService.Iface>(
+                new ThriftCourseServiceImpl());
+
+    }
+
+    private static TProtocolFactory createProtocolFactory() {
+        return new TBinaryProtocol.Factory();
+    }
 }
