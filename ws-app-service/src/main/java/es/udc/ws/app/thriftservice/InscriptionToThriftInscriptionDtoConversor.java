52b0cda964161644653e951f2957781c0def49d3
paulacarril <145045483+paulacarril@users.noreply.github.com>
2025-01-24 17:23:53 +0100

    [VARIOS] Implementado InscriptionToThriftInscriptionDtoConversor

--- ws-app-service/src/main/java/es/udc/ws/app/thriftservice/InscriptionToThriftInscriptionDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/app/thriftservice/InscriptionToThriftInscriptionDtoConversor.java
@@ -1,4 +1,26 @@
 package es.udc.ws.app.thriftservice;
 
+import es.udc.ws.app.model.inscription.Inscription;
+import es.udc.ws.app.thrift.ThriftInscriptionDto;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InscriptionToThriftInscriptionDtoConversor {
+    public static ThriftInscriptionDto toThriftInscriptionDto(Inscription inscription) {
+
+        return new ThriftInscriptionDto(inscription.getInscriptionId(), inscription.getCourseId(), inscription.getInscriptionDate(), inscription.getCancelationDate(),inscription.getUserEmail(),inscription.getCreditCard());
+
+    }
+
+    public static List<ThriftInscriptionDto> toThriftInscriptionDtos(List<Inscription> inscriptions) {
+
+        List<ThriftInscriptionDto> dtos = new ArrayList<>(inscriptions.size());
+
+        for (Inscription inscription: inscriptions){
+            dtos.add(toThriftInscriptionDto(inscription));
+        }
+
+        return dtos;
+
+    }
 }
