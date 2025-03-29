8b4bcd61bdce9cb7e574a462e94a014065476065
daniqss <danielqueijo14@gmail.com>
2025-03-29 10:05:43 +0100

    pdi: p5 ej 1

--- pdi/p5/p5.py
+++ pdi/p5/p5.py
@@ -2,23 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as sp
 
-
-valini = 0
 fs = 22100
 duracion = 22100
-n = np.arange(valini, duracion) / fs
-
-media = 0
-sigma = 1
-lx = len(n)
+n = np.arange(duracion) / fs
 
 f = 1000
-Na = 20
-L = lx
+Na = 10
+L = len(n)
 d = 1 / fs
+media = 0
+sigma = 1
 
 xcos = np.cos(2 * np.pi * f * n)
-for k in range(1, Na + 1):
+for k in range(1, Na):
     xcos += np.cos(2 * np.pi * (k + 1) * f * n)
 
 X = sp.fft(xcos, L)
@@ -26,22 +22,24 @@ freqs = np.fft.fftfreq(L, d)
 X_shifted = sp.fftshift(X)
 freqs_shifted = np.fft.fftshift(freqs)
 
-plt.stem(n, xcos)
-plt.xlabel("Frecuencia (Hz)")
-plt.ylabel("Magnitud")
-plt.title("DFT de la señal sin ruido")
-plt.show()
-
-xn = np.random.normal(media, sigma, lx)
-x = xcos + xn
-
-X = sp.fft(xcos, L)
-freqs = np.fft.fftfreq(L, d)
-X_shifted = sp.fftshift(X)
-freqs_shifted = np.fft.fftshift(freqs)
-
+plt.figure(figsize=(10, 4))
 plt.stem(freqs_shifted, np.abs(X_shifted))
 plt.xlabel("Frecuencia (Hz)")
 plt.ylabel("Magnitud")
+plt.title("DFT de la señal sin ruido")
+plt.grid()
+plt.show()
+
+xn = np.random.normal(media, sigma, L)
+x = xcos + xn
+
+X_noisy = sp.fft(x, L)
+X_noisy_shifted = sp.fftshift(X_noisy)
+
+plt.figure(figsize=(10, 4))
+plt.stem(freqs_shifted, np.abs(X_noisy_shifted))
+plt.xlabel("Frecuencia (Hz)")
+plt.ylabel("Magnitud")
 plt.title("DFT de la señal con ruido")
+plt.grid()
 plt.show()
