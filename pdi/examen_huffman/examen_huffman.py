24ff9a5774b5b5f2bf8c213c49a0fa68f6140aae
daniqss <danielqueijo14@gmail.com>
2025-05-09 10:39:09 +0200

    pdi: examen huffman

--- pdi/examen_huffman/examen_huffman.py
+++ pdi/examen_huffman/examen_huffman.py
@@ -1,18 +1,17 @@
 import numpy as np
 
 N = 4
-X = np.matrix([[1, 1],
-               [1, 1]])
+A = np.matrix([[1, 1],
+               [1, -1]])
 a = 1 / 2 * np.sqrt(2)
-A = np.matrix([
-    [0, 0, 1, 0],
-    [0, 0, 0, 1],
-    [0, 1, 0, 0],
-    [1, 0, 0, 0]
+X = np.matrix([
+    [1, 1, 2, 2],
+    [1, 1, 2, 2],
+    [1, 1, 3, 1],
+    [1, 1, 0, 0]
 ])
-# A = np.random.randint(0, 2, size=(4, 4))
 
-# A = a * A
+A = a * A
 I = np.eye(A.shape[0])
 
 assert A.shape[0] % 2 == 0, "error must be even rows"
@@ -24,10 +23,10 @@ print(f"X:\n {X}\n")
 block_size = 2
 result = ""
 
-for i in range(0, A.shape[0], block_size):
-    for j in range(0, A.shape[1], block_size):
-        Ai = A[i:i+block_size, j:j+block_size]
-        Fi = Ai @ X @ Ai.T
+for i in range(0, X.shape[0], block_size):
+    for j in range(0, X.shape[1], block_size):
+        Xi = X[i:i+block_size, j:j+block_size]
+        Fi = A @ Xi @ A.T
         
         print(f"Block ({i}:{i+2}, {j}:{j+2}):")
         print(np.array2string(Fi, precision=3, suppress_small=True), "\n")
